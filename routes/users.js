00c7577bc88566a84267640f33b900b08ea72493
alexmitse <alex1998mitsevich@gmail.com>
2019-12-13 19:26:52 +0300

    connection to database

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/users", (req, res, next) => {
+router.get("/", (req, res, next) => {
   return res.status(200).json({
     users: ["dsadas", "dsaasd"]
   });
