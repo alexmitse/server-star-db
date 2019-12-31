975916c33f83aded61ec35910a5e3860f948e70d
alexmitse <alex1998mitsevich@gmail.com>
2019-12-31 17:00:38 +0300

    add seeder species

--- database/models/species.js
+++ database/models/species.js
@@ -12,27 +12,3 @@ const species = sequelize.define(
   {}
 );
 module.exports = species;
-
-// module.exports = (sequelize, DataTypes) => {
-//   const species = sequelize.define(
-//     "species",
-//     {
-//       name: DataTypes.STRING,
-//       classification: DataTypes.STRING,
-//       language: DataTypes.STRING
-//     },
-//     {}
-//   );
-//   species.associate = function(models) {
-//     console.log(models);
-//     species.hasMany(models.people, {
-//       as: "people"
-//     });
-//     species.belongsToMany(models.films, {
-//       through: models.films_species,
-//       as: "films",
-//       foreignKey: "speciesId"
-//     });
-//   };
-//   return species;
-// };
